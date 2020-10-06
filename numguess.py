ed44195b3ec83100f56c86ecc3e2d8811999d29f
Chen Shi <cshi6@outlook.com>
2020-10-06 17:47:29 +0800

    fix typo

--- numguess.py
+++ numguess.py
@@ -2,12 +2,12 @@ import random
 secret = random.randint(1, 99)
 guess = 0
 tries = 0
-print("AHOY!  l'm thedread pirate roderts,and l have a secret!")
-print("lt is a number from 1 to 99. l'11 give you 6 tries.")
+print("AHOY!  I'm thedread pirate roderts, and I have a secret! ")
+print("It is a number from 1 to 99. I'll give you 6 tries.")
 while guess != secret  and tries < 6:
     guess = int(input("what's yer guess? "))
     if guess < secret: 
-        print( "Too low, ye scurvydog!")
+        print( "Too low, ye scurvy dog!")
     elif guess > secret:
         print("Too high, landlubber!")
 
@@ -16,6 +16,5 @@ while guess != secret  and tries < 6:
 if guess == secret:
     print("Avat! ye got it! found my secret, ye did!")
 else:
-    print("no more guesses! better luck next time,matey")
-    print("the secret number was", secret)                
-    
+    print("No more guesses! better luck next time,matey")
+    print("The secret number was", secret)                
